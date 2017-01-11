4b72d73b779fd9e0e0ec267a482e1934dcbf6b50
algattik <agattiker@gmail.com>
2017-01-11 12:37:16 +0100

    Removed dead code, improved instructions

--- Scripts/150-create-function.ps1
+++ Scripts/150-create-function.ps1
@@ -4,6 +4,8 @@ $scriptDir=($PSScriptRoot, '.' -ne "")[0]
 
 Write-Host "Create a Function App..."  -ForegroundColor Green
 
+$eventHubSendPolicyKey=(Get-AzureRmEventHubNamespaceKey -ResourceGroupName $resourceGroupName -NamespaceName $eventHubNamespace -AuthorizationRuleName $eventHubSendPolicyName).PrimaryConnectionString
+
 Write-Host -ForegroundColor Magenta "Perform this manual configuration"
 Write-Host -ForegroundColor Magenta "`nNavigate to:"
 $sub=(Get-AzureRmContext).Subscription.SubscriptionId
@@ -12,54 +14,7 @@ Write-Host -ForegroundColor Magenta "`nCreate new resource of type Function App,
 Write-Host -ForegroundColor Magenta "`nClick New Function -> GenericWebHook-CSharp -> Name your function '$functionName'. Paste following code: "
 Write-Host -ForegroundColor Cyan    (Get-Content -Path ..\Resources\PostToEventHubFunction\run.csx -Raw)
 
-#Doesn't work correctly
-function createFunction() {
-
-$templateFilePath = "..\Resources\PostToEventHubFunction\functionAppTemplate.json"
-if (-Not (Get-AzureRmWebApp  -ResourceGroupName $resourceGroupName -Name $functionAppName -ErrorAction SilentlyContinue)) {
-    New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $templateFilePath -TemplateParameterObject @{location=$location; storageAccount=$functionAppName; functionApp=$functionAppName}
-}
-
-Write-Host "Create a Function..."  -ForegroundColor Green
-
-$props = @{
-    config = @{
-        bindings = @(
-            @{
-                direction = "in"
-                name = "req"
-                type = "httpTrigger"
-                webHookType = "genericJson"
-            }
-            @{
-                direction = "out"
-                name = "res"
-                type = "http"
-            }
-            @{
-                connection = "$eventHubName"
-                direction = "out"
-                name = "outputEventHubMessage"
-                path = "$eventHubName"
-                type = "eventHub"
-            }
-        )
-    }
-    files = @{
-		"run.csx" = "$(Get-Content -Path ..\Resources\PostToEventHubFunction\run.csx -Raw)"
-	}
-}
-
-New-AzureRmResource -ResourceGroupName $resourceGroupName -ResourceType Microsoft.Web/sites/functions -ResourceName $functionAppName/$functionName -PropertyObject $props -ApiVersion 2015-08-01 -Force
-
-$sub=(Get-AzureRmContext).Subscription.SubscriptionId
-$eventHubSendPolicyKey=(Get-AzureRmEventHubNamespaceKey -ResourceGroupName $resourceGroupName -NamespaceName $eventHubNamespace -AuthorizationRuleName $eventHubSendPolicyName).PrimaryConnectionString
-Write-Host -ForegroundColor Magenta "Perform this manual configuration"
-Write-Host -ForegroundColor Magenta "`nNavigate to:"
-Write-Host -ForegroundColor Magenta "https://portal.azure.com/#blade/WebsitesExtension/FunctionsIFrameBlade/id/%2Fsubscriptions%2F$sub%2FresourceGroups%2F$resourceGroupName%2Fproviders%2FMicrosoft.Web%2Fsites%2F$functionAppName"
-Write-Host -ForegroundColor Magenta "`nClick PostToEventHub -> Integrate -> Configure event hub:"
-}
-
+Write-Host -ForegroundColor Magenta "`nClick -> Integrate -> + New Output -> Azure Event Hub. Configure connection:"
 Write-Host -ForegroundColor Magenta "`nEvent hub name:"
 Write-Host -ForegroundColor Cyan     $eventHubName
 Write-Host -ForegroundColor Magenta "Event hub connection: new, Connection name:"
